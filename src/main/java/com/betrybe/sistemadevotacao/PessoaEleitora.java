49e9a051e02b25436e91e6c0f293c7010d3166bc
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-09-30 16:45:15 -0300

    Req. 3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -4,6 +4,23 @@ package com.betrybe.sistemadevotacao;
  * Classe - Pessoa Eleitora.
  */
 
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /**
+   * Constructor.
+   */
+
+  public PessoaEleitora(String nome, String cpf) {
+    super(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
