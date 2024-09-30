1bc68b74ffa7b5c8b1ffed54382b9bcc192e9d50
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-09-30 15:29:13 -0300

    Req 1 + Req 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -5,5 +5,33 @@ package com.betrybe.sistemadevotacao;
  */
 
 public class PessoaCandidata {
+  public String nome;
+  public int numero;
+  public int votos;
 
+  /**
+   * Constructor.
+   */
+
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void recebeVoto() {
+    this.votos++;
+  }
 }
